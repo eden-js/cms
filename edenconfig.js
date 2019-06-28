2015290cd072151f2c4791f3c088a94f8115919a
installgen2 <gen2@gen2.space>
2019-06-27 21:14:46 -0700

    import summernote to js and css

---
+++ edenconfig.js
@@ -0,0 +1,11 @@
+const config = {};
+
+config.js = [
+  './node_modules/summernote/dist/summernote-bs4.js',
+];
+
+config.sass = [
+  './node_modules/summernote/dist/summernote-bs4.css',
+];
+
+module.exports = config;
